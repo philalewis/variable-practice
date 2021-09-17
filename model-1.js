f6080c0682c580d83177dc5db1b2fd40d1297938
Phil Lewis <phillewis1985@gmail.com>
2021-09-16 21:21:24 -0500

    Add object

--- model-1.js
+++ model-1.js
@@ -9,3 +9,10 @@ var isAutoTrans = true;
 
 var firstArray = ["gray", 'Firestone Tires'];
 var secondArray = [2.6, true, "reliable", 'fast'];
+
+var object = {
+  make: "Honda",
+  model: "Accord",
+  year: 2006,
+  color: "gray"
+};
